c67f1cb25b13ec953beb20162fe5900e64b780d0
dasveloper <28981975+dasveloper@users.noreply.github.com>
2022-06-07 13:46:19 -0400

    Relative paths

--- utils/fileReader.js
+++ utils/fileReader.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
+import path from 'path';
 
 export const readFileData = (file) => {
-  const data = fs.readFileSync(file, 'utf-8').split('\r\n');
+  const filePath = path.join(process.cwd(), file);
+  const data = fs.readFileSync(filePath, 'utf-8').split('\r\n');
   data.pop();
   return data;
 };
 
 export const writeFileData = (file, data) => {
-  fs.appendFileSync(file, data);
+  const filePath = path.join(process.cwd(), file);
+  fs.appendFileSync(filePath, data);
 };
